0d263b4ea75a182dd5a01cae319ad23a4ae0bec0
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-05-19 00:11:29 +0600

    resize navbar and banner

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   return (
     <div className="nav">
       <h3 className="header">DreamHub</h3>
-      <a href="">Statistics</a>
-      <a href="">Applied Jobs</a>
-      <a href="">Blog</a>
+      <div className="link">
+        <a href="">Statistics</a>
+        <a href="">Applied Jobs</a>
+        <a href="">Blog</a>
+      </div>
       <button className="btn">Star Applying</button>
     </div>
   );
